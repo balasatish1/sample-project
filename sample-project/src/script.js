2d077e3b2be9b2278a2fec0ad338405fe93dbaf7
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-06-24 18:17:27 +0530

    add the functioality of the project

--- sample-project/src/script.js
+++ sample-project/src/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const submitBtnClickEvent = () => {
+  const name = document.querySelector('.name-input').value;
+  const paraElement = document.createElement('p');
+  paraElement.innerText='I love you ' + name;
+  mainContainer = document.querySelector('main').appendChild(paraElement);
+}
 
-
-
-
-
-
-const main = function() {
-  console.log('You are in main function...');
+const main = function() {  
+  const submitBtn =document.querySelector('.submit-btn');
+  submitBtn.addEventListener('click', submitBtnClickEvent);
 }
 
 window.onload = main;
