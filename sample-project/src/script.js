815ec1374de993df5a2b364e826c7423a5b72cee
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-06-24 18:18:18 +0530

    remove the unwanted varible

--- sample-project/src/script.js
+++ sample-project/src/script.js
@@ -4,7 +4,7 @@ const submitBtnClickEvent = () => {
   const name = document.querySelector('.name-input').value;
   const paraElement = document.createElement('p');
   paraElement.innerText='I love you ' + name;
-  mainContainer = document.querySelector('main').appendChild(paraElement);
+  document.querySelector('main').appendChild(paraElement);
 }
 
 const main = function() {  
